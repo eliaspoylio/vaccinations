63e4d6cf1729b487f1e2ae2e4090191a71118e07
elias <elias.poylio@gmail.com>
2021-07-01 21:38:17 +0300

    add pytest

--- backend/app/app.py
+++ backend/app/app.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from .database import SessionLocal, engine
 from sqlalchemy.orm import Session
-from sqlalchemy import text, func, select
+from sqlalchemy import text, func, select, cast, DATE
 
 from . import models, schemas
 from typing import List
@@ -39,17 +39,23 @@ async def root():
     return {"backend": "Hello world!"}
 
 @app.get("/orders/", response_model=List[schemas.Order])
-def show_records(db: Session = Depends(get_db)):
+def show_orders(db: Session = Depends(get_db)):
     orders = db.query(models.Order).all()
     return orders
 
 @app.get("/vaccinations/", response_model=List[schemas.Vaccination])
-def show_records(db: Session = Depends(get_db)):
+def show_vaccinations(db: Session = Depends(get_db)):
     vaccinations = db.query(models.Vaccination).all()
     return vaccinations
 
 @app.get("/injections/{day}")
-def show_records(day, db: Session = Depends(get_db)):
+def show_injections_arrived(day, db: Session = Depends(get_db)):
     statement = select(func.sum(models.Order.injections)).where(models.Order.arrived<day)
+    result = db.execute(statement).all()
+    return result
+
+@app.get("/orders/day/{day}")
+def show_orders_arrived_day(day, db: Session = Depends(get_db)):
+    statement = select(func.count(models.Order.id)).where(cast(models.Order.arrived, DATE)==day)
     result = db.execute(statement).all()
     return result
---
+++ backend/tests/test_app.py
@@ -0,0 +1,26 @@
+from fastapi.testclient import TestClient
+
+from app.app import app
+
+client = TestClient(app)
+
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"backend": "Hello world!"}
+
+def test_show_injections_arrived():
+    response = client.get("/injections/2021-04-12T11:10:06")
+    assert response.status_code == 200
+    assert response.json() == [{"sum": 25015}]
+
+def test_show_orders_arrived_total():
+    response = client.get("/orders/sum")
+    assert response.status_code == 200
+    assert response.json() == [{"sum": 5000}]
+
+def test_show_orders_arrived_day():
+    response = client.get("/orders/day/2021-03-20")
+    assert response.status_code == 200
+    assert response.json() == [{"count": 61}]
