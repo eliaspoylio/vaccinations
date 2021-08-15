c408d6f91a48fd36c16c28a3dcf360dc459da57b
elias <elias.poylio@gmail.com>
2021-08-15 18:49:47 +0300

    add env defaults to backend db connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,10 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-POSTGRES_USER = os.environ["POSTGRES_USER"]
-POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
-POSTGRES_URL = os.environ["POSTGRES_URL"]
-POSTGRES_DB = os.environ["POSTGRES_DB"]
+POSTGRES_USER = os.environ.get("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "postgres")
+POSTGRES_URL = os.environ.get("POSTGRES_URL", "postgres:5432")
+POSTGRES_DB = os.environ.get("POSTGRES_DB", "vaccinations")
 
 DB_URL = "postgresql://{user}:{pw}@{url}/{db}".format(user=POSTGRES_USER,pw=POSTGRES_PASSWORD,url=POSTGRES_URL,db=POSTGRES_DB)
 
